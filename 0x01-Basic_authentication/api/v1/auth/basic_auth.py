48b9c13af0c6e5b73970dae80b22796f511d6a3f
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-09-10 09:29:44 +0300

    Decode a base64 header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """API Authentication Module"""
 from . import auth
+import base64
 
 
 class BasicAuth(auth.Auth):
@@ -17,3 +18,19 @@ class BasicAuth(auth.Auth):
         encoded_string = authorization_header.split(" ", 1)[1]
 
         return encoded_string
+    
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """Encode data"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            encoded_str = base64_authorization_header.encode('utf-8')
+            decoded_str_b64 = base64.b64decode(encoded_str)
+            decoded = decoded_str_b64.decode('utf-8')
+        except BaseException:
+            return None
+        
+        return decoded
