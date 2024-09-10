ad5c1c1b6a0376c6b838520b0f6488f96d4f76d3
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-09-10 09:42:49 +0300

    Extract user details from db

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """API Authentication Module"""
 from . import auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(auth.Auth):
@@ -48,3 +50,22 @@ class BasicAuth(auth.Auth):
         email, password = decoded_base64_authorization_header.split(':', 1)
 
         return email, password
+
+    def user_object_from_credentials(self, user_email:
+                                     str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """Get user object from credentials"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users_db = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in users_db:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
