aabdb5e34529f4c4015d7768e6efbf99ca541ba6
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-09-10 08:41:33 +0300

    Fix codestyle issues

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,7 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require Auth Method"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """Validate all requests"""
         if request is None:
@@ -18,7 +18,7 @@ class Auth:
         if auth is None:
             return None
         return auth
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Return the current user"""
         return None
