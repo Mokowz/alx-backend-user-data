2cce9e2c481f008c8d28d1ba34f43577b2105c3e
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-09-10 09:19:38 +0300

    base64 encode data

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,15 @@ from . import auth
 
 class BasicAuth(auth.Auth):
     """Basic Auth Class"""
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """Extract base64 authorization"""
-        pass
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        encoded_string = authorization_header.split(" ", 1)[1]
+
+        return encoded_string
