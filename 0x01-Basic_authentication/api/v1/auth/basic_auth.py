9ae6a28c94362fc8f4bd5f8bc1ce36d89f58ce51
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-09-10 09:14:30 +0300

    Fix indentation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,4 @@ class BasicAuth(auth.Auth):
     """Basic Auth Class"""
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """Extract base64 authorization"""
+        pass
