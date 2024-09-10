4f25eacb2f471e5dfcb3a73b586d80575a17b9a7
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-09-10 09:35:42 +0300

    Extract user details

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,3 +34,17 @@ class BasicAuth(auth.Auth):
             return None
 
         return decoded
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """Extract user credentials"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        
+        email, password = decoded_base64_authorization_header.split(':')
+
+        return email, password
