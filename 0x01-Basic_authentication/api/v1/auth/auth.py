6502b463f0235b6141bbd32378d733ce5851529c
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-09-10 08:35:48 +0300

    Add the existing files

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""API Authentication Module"""
+from flask import request
+from typing import TypeVar, List
+
+
+class Auth:
+    """Authentication class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Validate all requests"""
+        if request is None:
+            return None
+        auth = request.headers.get('Authorization')
+        if auth is None:
+            return None
+        return auth
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
--- 0x01-Basic_authentication/ex.py
+++
@@ -1,4 +0,0 @@
-import base64
-
-encoded = base64.b64encode(b'Hey There I love Whatsapp')
-print(f'Encoded Msg: {encoded}')
